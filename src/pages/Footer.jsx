0444582b9a26ba9163b216e23cc5d20234dfda33
nikhil969rockss <ns9013604867@gmail.com>
2025-06-27 23:52:53 +0530

    updated footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,17 +5,19 @@ const Footer = () => {
   return (
     <>
       <div className="w-full md:h-[100vh] h-[70vh] bg-transparent"></div>
-        
-      
+
       <div className="text-white bg-black w-full md:h-screen h-[70vh] fixed bottom-0 left-0 flex justify-center items-end z-[1] p-4 ">
         <div className="desktop-balls hidden md:block">
-          <Ball  className={"left-[-20%] top-[-40%] w-[80vw] h-[60vw]" } />
-          <Ball className={"left-[50%]  -translate-x-1/2  top-[-70%] w-[60vw] h-[70vw]"} />
-          <Ball   className={"right-[-10%]  top-[-40%] w-[90vw] h-[50vw]"} />
+          <Ball className={"left-[-20%] top-[-40%] w-[80vw] h-[60vw]"} />
+          <Ball
+            className={
+              "left-[50%]  -translate-x-1/2  top-[-70%] w-[60vw] h-[70vw]"
+            }
+          />
+          <Ball className={"right-[-10%]  top-[-40%] w-[90vw] h-[50vw]"} />
         </div>
         <div className="mobile-balls md:hidden">
           <Ball className={"w-[50vh] h-[40vh] top-[-10%] "} />
-          
         </div>
         <div className="email-div absolute w-[80%] md:w-[90%] md:p-4 top-[4%]  mt-5 md:mt-10 flex justify-between items-center  ">
           <div className="flex flex-col justify-center font-[neu1] text-[4vh] leading-[4vh] gap-2 ">
@@ -24,9 +26,17 @@ const Footer = () => {
             <p>contact</p>
           </div>
           <div className=" w-[60%] font-[neu2] md:w-[30%] ">
-            <p className="text-[2vh] md:text-[1.5vw] md:leading-[1.5vw] leading-[2.4vh]">Get industry insights and creative inspiration straight to your inbox.</p>
+            <p className="text-[2vh] md:text-[1.5vw] md:leading-[1.5vw] leading-[2.4vh]">
+              Get industry insights and creative inspiration straight to your
+              inbox.
+            </p>
             <div className="flex items-end justify-between border-b border-white/40">
-              <input className="mt-5 outline-none  bg-transparent text text-sm md:text-md w-full " type="text" placeholder="Email address" /><i className="ri-arrow-right-line cursor-pointer"></i>
+              <input
+                className="mt-5 outline-none  bg-transparent text text-sm md:text-md w-full "
+                type="text"
+                placeholder="Email address"
+              />
+              <i className="ri-arrow-right-line cursor-pointer"></i>
             </div>
           </div>
         </div>
@@ -36,14 +46,18 @@ const Footer = () => {
           </h1>
           <div className="w-full  h-[0.5px] bg-white/50"></div>
           <div className="capitalize flex gap-2 justify-between text-sm md:text-md mt-4 text-white/80">
-            <a>Copyright&copy;sundown studio</a>
+            <a>Copyright&copy;sundown studio by Nikhil R </a>
             <a>
               <i className="ri-global-line"></i>Brooklyn, NY
             </a>
             <a href="#" className="cursor-pointer">
               <i className="ri-instagram-line"></i>Instagram
             </a>
-            <a target="_blank" href="https://www.linkedin.com/in/nikhil-randhawa-9a197720b" className="cursor-pointer">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/nikhil-randhawa-9a197720b"
+              className="cursor-pointer"
+            >
               <i className="ri-linkedin-box-fill"></i>LinkedIn
             </a>
           </div>
